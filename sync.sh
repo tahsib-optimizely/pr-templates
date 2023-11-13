0a7ff16aa86a0d8318b12de891153f39dfcfff1a
Tahsib Ahad Chowdhury <tahsib.ahad@optimizely.com>
2023-11-13 22:04:18 +0600

    updates added

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #git submodule add https://github.com/tahsib-optimizely/pr-templates.git pr-templates
 mkdir -p .github/
 mv -f ./$1/pull_request_template.md .github/pull_request_template.md
-mv -f "./$1/"* ../
+mv -f "./$1"/* ../
