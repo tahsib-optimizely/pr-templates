dc269146b9244252d4b02f22c41c769916c8bd07
Tahsib Ahad Chowdhury <tahsib.ahad@optimizely.com>
2023-11-14 20:38:57 +0600

    converted to action

--- cleanup.sh
+++
@@ -1,7 +0,0 @@
-echo "**** Cleaning up submodules ****"
-git submodule deinit -f -- pr-templates    
-git rm --cached pr-templates    
-rm -rf .git/modules/pr-templates
-rm -f .gitmodules
-rm -rf pr-templates
-echo "**** Cleaning Complete! ****"
